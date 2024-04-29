1aa0b265fddfd26c2768938b6ff122978de1729e
NikolaJelavic <jelavicnikola@gmail.com>
2024-04-29 21:00:47 +0200

    colors

--- src/pages/Two.jsx
+++ src/pages/Two.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 
 const NoteApp = () => {
     const [notes, setNotes] = useState([]);
@@ -33,6 +33,13 @@ const NoteApp = () => {
             localStorage.setItem('notes', JSON.stringify(notesObj));
             displayNotes();
         }
+    };
+
+    const changeColor = (index, color) => {
+        let notesObj = [...notes];
+        notesObj[index].color = color;
+        localStorage.setItem('notes', JSON.stringify(notesObj));
+        displayNotes();
     };
 
     const addNote = () => {
@@ -73,12 +80,17 @@ const NoteApp = () => {
                     <h3 style={{ textAlign: 'center', color: 'grey' }}>Nothing to display</h3>
                 ) : (
                     notes.map((note, index) => (
-                        <div key={index} className="card mx-4 my-2 bg-dark text-white thatsMyNote" style={{ width: '18rem' }}>
+                        <div key={index} className="card mx-4 my-2" style={{ width: '18rem', backgroundColor: note.color || 'white', position: 'relative' }}>
                             <div className="card-body">
                                 <h6>{note.time}</h6>
                                 <p className="card-text">{note.text}</p>
-                                <button onClick={() => editNote(index)} className="btn btn-primary mr-2">Edit</button>
-                                <button onClick={() => deleteNote(index)} className="btn btn-danger">Delete</button>
+                                <div className="absolute top-0 right-0 mt-2 mr-2 ">
+                                    <input type="color" value={note.color || '#ffffff'} onChange={(e) => changeColor(index, e.target.value)} />
+                                </div>
+                                <div className="d-flex justify-content-between">
+                                    <button onClick={() => editNote(index)} className="btn btn-primary">Edit</button>
+                                    <button onClick={() => deleteNote(index)} className="btn btn-danger">Delete</button>
+                                </div>
                             </div>
                         </div>
                     ))
